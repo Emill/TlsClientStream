c026dee63988fa03f184bb032699eafb514ea515
Emil Lenngren <emil.lenngren@gmail.com>
2015-03-01 23:49:05 +0100

    Support for TLS 1.0 and 1.1

--- TlsClientStream/CipherSuiteInfo.cs
+++ TlsClientStream/CipherSuiteInfo.cs
@@ -60,6 +60,27 @@ namespace TlsClientStream
             }
         }
 
+        public SignatureAlgorithm GetSignatureAlgorithm()
+        {
+            switch (KeyExchange)
+            {
+                case KeyExchange.DHE_DSS:
+                    return SignatureAlgorithm.DSA;
+                case KeyExchange.DHE_RSA:
+                case KeyExchange.ECDH_RSA:
+                case KeyExchange.ECDHE_RSA:
+                    return SignatureAlgorithm.RSA;
+                case KeyExchange.ECDH_ECDSA:
+                case KeyExchange.ECDHE_ECDSA:
+                    return SignatureAlgorithm.ECDSA;
+                case KeyExchange.RSA:
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public bool IsAllowedBefore1_2 { get { return AesMode == AesMode.CBC && (ushort)Id < 0xC023; } }
+
         public static readonly CipherSuiteInfo[] Supported = new CipherSuiteInfo[] {
             new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384, KeyExchange = KeyExchange.ECDHE_RSA, AesKeyLen = 256, HashAlgorithm = TLSHashAlgorithm.SHA384, PRFAlgorithm = PRFAlgorithm.TLSPrfSHA384 },
             new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA, KeyExchange = KeyExchange.ECDHE_RSA, AesKeyLen = 256, HashAlgorithm = TLSHashAlgorithm.SHA1 },
@@ -75,7 +96,7 @@ namespace TlsClientStream
             new CipherSuiteInfo() { Id = CipherSuite.TLS_DHE_DSS_WITH_AES_256_CBC_SHA, KeyExchange = KeyExchange.DHE_DSS, AesKeyLen = 256, HashAlgorithm = TLSHashAlgorithm.SHA1 },
             new CipherSuiteInfo() { Id = CipherSuite.TLS_DHE_DSS_WITH_AES_128_CBC_SHA256, KeyExchange = KeyExchange.DHE_DSS, AesKeyLen = 128, HashAlgorithm = TLSHashAlgorithm.SHA256 },
             new CipherSuiteInfo() { Id = CipherSuite.TLS_DHE_DSS_WITH_AES_128_CBC_SHA, KeyExchange = KeyExchange.DHE_DSS, AesKeyLen = 128, HashAlgorithm = TLSHashAlgorithm.SHA1 },
-            new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, KeyExchange = KeyExchange.ECDHE_RSA, AesKeyLen = 256, HashAlgorithm = TLSHashAlgorithm.SHA384, PRFAlgorithm = PRFAlgorithm.TLSPrfSHA384 },
+            new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, KeyExchange = KeyExchange.ECDHE_RSA, AesKeyLen = 256, AesMode = AesMode.GCM, PRFAlgorithm = PRFAlgorithm.TLSPrfSHA384 },
             new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, KeyExchange = KeyExchange.ECDHE_RSA, AesKeyLen = 128, AesMode = AesMode.GCM },
             new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, KeyExchange = KeyExchange.ECDHE_ECDSA, AesKeyLen = 256, AesMode = AesMode.GCM, PRFAlgorithm = PRFAlgorithm.TLSPrfSHA384 },
             new CipherSuiteInfo() { Id = CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, KeyExchange = KeyExchange.ECDHE_ECDSA, AesKeyLen = 128, AesMode = AesMode.GCM },
